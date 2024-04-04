15dcf64a7c97f9a67a51def62499745b449a838e
alutterbein <alutterbein@gmail.com>
2024-04-03 20:45:13 -0700

    finished usertable

--- models/User.js
+++ models/User.js
@@ -64,8 +64,19 @@ Users.init(
 
         },
     },
+{
+    hooks: {
+        beforeCreate:async (newUserData) => {
+            newUserData.password =await bcrypt.hash(newUserData.password, 10)
+            return newUserData;
+        },
+    },
+    sequelize, 
+    timestamps: false,
+    freezeTablName: true,
+    underscored: true,
+    modelName: 'user',
 
+});
 
-
-
-)
+module.exports = User;

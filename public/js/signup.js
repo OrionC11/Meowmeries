7c6ac3d357e094fac6e33e452d53396cf24e1232
wellheythereMJ <abetterplace@outlook.com>
2024-04-10 19:15:41 -0700

    signup.js changes

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,17 +3,17 @@ const signupFormHandler = async (event) => {
 
   const username = document.querySelector('#su-user').value.trim();
   const password = document.querySelector('#su-pass').value.trim();
-  const firstName = document.querySelector('#su-FName').value.trim();
-  const lastName = document.querySelector('#su-LName').value.trim();
+  const first_name = document.querySelector('#su-FName').value.trim();
+  const last_name = document.querySelector('#su-LName').value.trim();
   const email = document.querySelector('#su-email').value.trim();
   const catName = document.querySelector('#su-CName').value.trim();
   const catBreed = document.querySelector('#su-CBreed').value.trim();
   const catAge = document.querySelector('#su-CAge').value.trim();
 
-  if (username && password && firstName && lastName && email) {
+  if (username && password && first_name && last_name && email) {
     const response = await fetch('/api/user', {
       method: 'POST',
-      body: JSON.stringify({ username, password, firstName, lastName, email, catBreed, catAge, catName }),
+      body: JSON.stringify({ username, password, first_name, last_name, email, catBreed, catAge, catName }),
       headers: { 'Content-Type': 'application/json' },
     });
 
